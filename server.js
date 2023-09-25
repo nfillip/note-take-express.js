79ec13c14092a4da6a3069f919c02a19f07ffb51
nfillip <nick.m.fillip@gmail.com>
2023-09-25 09:36:57 -0600

    changed PORT path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 const app = express();
 const { clog } = require('./middleware/clog');
 const Cyan = '\x1b[36m';
